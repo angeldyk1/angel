b62c6a3e3a25507021982db079a8863f97c4a89e
angeldyk1 <148715906+angeldyk1@users.noreply.github.com>
2023-12-18 21:44:08 -0600

    php ejercicio
    Co-Authored-By: Carlos Sánchez Donate <43948976+barvock@users.noreply.github.com>
    Co-Authored-By: MrRobaboba <50331557+MrRobaboba@users.noreply.github.com>

---
+++ assets/functions.php
@@ -0,0 +1,18 @@
+<?php
+
+function ctas(){
+
+echo "Prueba número mil";
+
+
+}
+
+function pruebafuncionhtml(){
+
+    echo "<ul>
+    
+    <li>¿Cómo se hace una función?</li>
+    <li>¿Tú sabes realizar funciones?</li> </ul>";
+}
+
+?>
